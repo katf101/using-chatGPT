f731dea5992bf02a4271131f362c596444254051
Hansol <102638663+katf101@users.noreply.github.com>
2023-03-01 23:49:14 +0900

    moongoose 연결

--- Connect4/backend/server.js
+++ Connect4/backend/server.js
@@ -1,5 +1,12 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+
+// MongoDB 연결
+mongoose
+  .connect("mongodb://localhost:27017/my_database", { useNewUrlParser: true })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("Connect4 Game!!!");
